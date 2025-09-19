b17486b4f412a62879bce6ea986867ec138cef49
weiwei_hacking <118904132+weiwei-hacking@users.noreply.github.com>
2025-09-19 16:16:03 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,15 +18,6 @@ async def load_cogs():
         if filename.endswith('.py'):
             await bot.load_extension(f'cogs.{filename[:-3]}')
 
-DISCORD_TOKEN = os.getenv("BOT_TOKEN")
-HYPIXEL_API_KEY = os.getenv("HYPIXEL_API")
-
-if not DISCORD_TOKEN or not HYPIXEL_API_KEY:
-    with open("configs.json", "r", encoding="utf-8") as f:
-        config = json.load(f)
-        DISCORD_TOKEN = DISCORD_TOKEN or config.get("bot_token")
-        HYPIXEL_API_KEY = HYPIXEL_API_KEY or config.get("hypixel_api")
-
 @bot.event
 async def on_ready():
     await bot.change_presence(status=discord.Status.idle, activity=discord.Activity(type=discord.ActivityType.listening, name="Hypixel Punishment"))
@@ -36,3 +27,4 @@ async def on_ready():
 
 
 bot.run(config['bot_token'])
+
